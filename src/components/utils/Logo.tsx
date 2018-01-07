1a36984001a56f97e96b88b945e42a9a5b6a55f2
Marco del Carmen <jesusmarcodelcarmen@gmail.com>
2018-01-07 04:30:24 -0600

    moved public to s3

--- src/components/utils/Logo.tsx
+++ src/components/utils/Logo.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Logo = () => (
   <div className="logo">
     <span>Melting Pot</span>
-    <img width={40} src="../../../public/imgs/melting-pot.svg" />
+    <img width={40} src=" https://s3-us-west-2.amazonaws.com/melting-pot-images/melting-pot.svg" />
   </div>
 );
 
